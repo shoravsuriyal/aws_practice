6757c228388bfdf56ff3ceca1ff3eda3893ac121
sHor@v_macBook <newuser@Shorav-Macbook.local>
2020-10-21 03:41:20 -0700

    updated script file for lambda dynamodb

--- lambda/script.sh
+++ lambda/script.sh
@@ -1,1 +1,5 @@
 aws lambda invoke --function-name function_1 out.txt
+aws iam create-role --role-name lambda-dynamodb --assume-role-policy-document file://trust-policy.json
+aws lambda create-function --function-name function_1 --runtime python2.7 --zip-file fileb://function.zip --handler lambda_handler.lambda_handler --role arn:aws:iam::496585200392:role/lambda-dynamodb
+aws lambda delete-function --function-name function_1
+zip function.zip lambda_handler.py
